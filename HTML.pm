bb6dbbe30be9c485c57d3e703855735ab50a0005
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-08-02 01:16:58 +0200

    Prepare for 0.09 version

--- HTML.pm
+++ HTML.pm
@@ -7,7 +7,7 @@ use Class::Utils qw(set_params);
 use Error::Pure qw(err);
 use Scalar::Util qw(blessed);
 
-our $VERSION = 0.08;
+our $VERSION = 0.09;
 
 # Constructor.
 sub new {
@@ -482,6 +482,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.08
+0.09
 
 =cut
